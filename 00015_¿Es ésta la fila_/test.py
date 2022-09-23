0a5c2213e5e9886e333784abf3a2ebb8aa175a8c
franco@mumuki.org <franco@mumuki.org>
2022-09-23 19:39:24 +0000

    Mumuki Export on 2022-09-23 19:39:24 +0000

--- 00015_¿Es ésta la fila_/test.py
+++ 00015_¿Es ésta la fila_/test.py
@@ -1,4 +1,11 @@
 class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_primera_libreria_es_un_series(self):
+    self.assertEquals(type(primera_libreria), pd.Series)
+    
+  def test_segunda_y_tercera_libreria_es_un_dataFrame(self):
+    self.assertEquals(type(segunda_y_tercera_libreria), pd.DataFrame)
+        
+  def test_ultima_libreria_es_un_series(self):
+    self.assertEquals(type(ultima_libreria), pd.Series)
+    
