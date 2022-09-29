f91d410f1caabb9889e4c47094eff2913292cccb
franco@mumuki.org <franco@mumuki.org>
2022-09-29 01:31:13 +0000

    Mumuki Export on 2022-09-29 01:31:13 +0000

--- 00010_Haciendo valer el orden/test.py
+++ 00010_Haciendo valer el orden/test.py
@@ -1,4 +1,9 @@
+import pandas as pd
+
 class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_cines_mas_amplios_es_un_dataFrame(self):
+    self.assertEquals(type(cines_mas_amplios), pd.DataFrame)
+    
+  def test_cines_mas_amplios_tiene_tres_filas(self):
+    self.assertEquals(len(cines_mas_amplios), 3)    
